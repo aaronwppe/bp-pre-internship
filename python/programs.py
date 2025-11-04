b4d75078c901a4c43bd03332a3c629233b9127bb
Aaron Mathew <149572843+aaronwppe@users.noreply.github.com>
2025-11-04 11:14:07 +0530

    update programs.py

--- python/programs.py
+++ python/programs.py
@@ -1,7 +1,10 @@
-import random, sys
+import random, sys, csv
+
+file_name = "Learning Progress Tracker(Programs).csv"
+program_count = 974
 
 random.seed(0)
-programs = list(range(974))
+programs = list(range(program_count))
 random.shuffle(programs)
 
 n = int(sys.argv[1]) - 1
@@ -9,7 +12,26 @@ n = int(sys.argv[1]) - 1
 if n < 0:
     exit(1)
 
-if n < len(programs):
-    print(programs[n] + 1)
-else:
+elif n >= len(programs):
     print("EOF")
+    exit(0)
+
+random_index = programs[n] + 1
+python_file_name = f"{random_index}.py"
+comments = []
+
+with open(file_name, 'r') as file:
+    reader = csv.reader(file)
+    
+    for i in range(random_index + 1):
+        comments = next(reader)
+
+try:
+    with open(python_file_name, "x") as file:
+        for line in comments:
+            file.write(f"# {line}\n")
+    
+    print(f"Created: {python_file_name}")
+
+except FileExistsError:
+    print(f"Exists: {python_file_name}")
